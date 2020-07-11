d6d6bae7d62e23e3240ef205ddedbb75fe2edd75
alice-revelant <alice.byrnes@revelanttech.com>
2020-07-11 15:33:54 -0500

    Sort of functioning

--- spaghettiApp.py
+++ spaghettiApp.py
@@ -87,7 +87,6 @@ def upload_file():
 def uploaded_file(filename):
     os.system('cd /Users/ethannguyen/Desktop/College/SyBBURE/2020\ Summer/darknet; ./darknet detect cfg/model.cfg 3209.neg_32213.22300.weights /Users/ethannguyen/Desktop/College/SyBBURE/2020\ Summer/VHacksSummer/upload' + filename)
     os.system('mv /Users/ethannguyen/Desktop/College/SyBBURE/2020\ Summer/darknet/predictions.jpg /Users/ethannguyen/Desktop/College/SyBBURE/2020\ Summer/VHacksSummer/upload')
-    
     return send_from_directory(app.config['UPLOAD_FOLDER'],
                                'predictions.jpg')
 
