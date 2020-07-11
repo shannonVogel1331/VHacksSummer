c8face2b11e58fbf54708aca82f588e0bfc00416
alice-revelant <alice.byrnes@revelanttech.com>
2020-07-11 13:16:14 -0500

    Upload photo via python

--- spaghettiApp.py
+++ spaghettiApp.py
@@ -1,13 +1,22 @@
-from flask import Flask, render_template, url_for, request, redirect, flash
+from flask import Flask, render_template, url_for, request, redirect, flash, send_from_directory
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 from werkzeug.utils import secure_filename
+import os
+
+UPLOAD_FOLDER = 'upload'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///prints.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 db = SQLAlchemy(app)
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 class printPhoto(db.Model): # Feel free to add more attributes as y'all see fit
     id = db.Column(db.Integer, primary_key=True)
@@ -18,10 +27,6 @@ class printPhoto(db.Model): # Feel free to add more attributes as y'all see fit
     def __repr__(self):
         return '<Task %r>' % self.id
 
-
-@app.route('/', methods=['POST', 'GET'])
-def home():
-    return render_template('home.html')
 
 """
     if request.method == 'POST':
@@ -56,21 +61,28 @@ def troubleshootQuality():
         return 'There was a problem redirecting'
 
 
-@app.route('/upload', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
+        # check if the post request has the file part
         if 'file' not in request.files:
-            flash('No file part')
-            return redirect('/')
-
-        f = request.files['the_file']
+            return render_template('home.html', message="Please select a file.")
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
         if file.filename == '':
-            flash('No selected file')
-            return redirect('/')
+            return render_template('home.html', message="Please select a file.")
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file',
+                                    filename=filename))
+    return render_template('home.html', message="Upload")
 
-        f.save('/var/www/uploads/' + secure_filename(f.filename))
-
-        return ""
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+                               filename)
 
 if __name__ == "__main__":
     app.run(debug=True)
