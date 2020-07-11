bce764b5867aac9f5a92e139b97836da716964ef
alice-revelant <alice.byrnes@revelanttech.com>
2020-07-11 09:36:30 -0500

    Functioning? App

--- spaghettiApp.py
+++ spaghettiApp.py
@@ -1,9 +1,12 @@
-from flask import Flask, render_template, url_for, request, redirect
+from flask import Flask, render_template, url_for, request, redirect, flash
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///prints.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 db = SQLAlchemy(app)
 
 class printPhoto(db.Model): # Feel free to add more attributes as y'all see fit
@@ -13,10 +16,14 @@ class printPhoto(db.Model): # Feel free to add more attributes as y'all see fit
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
     
     def __repr__(self):
-        return '<Print %r>' % self.id
+        return '<Task %r>' % self.id
+
 
 @app.route('/', methods=['POST', 'GET'])
-def index():
+def home():
+    return render_template('home.html')
+
+"""
     if request.method == 'POST':
         img_path = request.form['content']  # Have this reference an upload form
         print_status = ""   # Machine output here
@@ -32,10 +39,31 @@ def index():
     else:
         prints = printPhoto.query.order_by(printPhoto.date_created).all()
         return render_template('home.html', prints=prints)
+"""
 
 @app.route('/troubleshoot')
 def troubleshoot():
     try:
-        return redirect('/troubleshoot')
+        return render_template('troubleshooting.html')
     except:
         return 'There was a problem redirecting'
+
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect('/')
+
+        f = request.files['the_file']
+        if file.filename == '':
+            flash('No selected file')
+            return redirect('/')
+
+        f.save('/var/www/uploads/' + secure_filename(f.filename))
+
+        return ""
+
+if __name__ == "__main__":
+    app.run(debug=True)
